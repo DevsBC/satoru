2e2866beddcc62ac6be26f52f37a25b07cd24cbf
ltrbomb10 <ltrbomb10@ltrbomb10.debian>
2018-10-30 19:25:08 -0700

    Agregado varias cosas que serviran de base para la pagina web

---
+++ includes/templates/footer.php
@@ -0,0 +1,27 @@
+<footer class="site-footer">
+  <div class="contenedor clearfix">
+    <div class="footer-informacion">
+        <h3>Sobre <span>Satoru</span></h3>
+        <p>Lorem ipsum dolor sit amet, consectetur</p>
+    </div>
+    <div class="ultimos-tweets">
+        <h3>Ultimos <span>tweets</span></h3>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur</li>
+          <li>Lorem ipsum dolor sit amet, consectetur</li>
+          <li>Lorem ipsum dolor sit amet, consectetur</li>
+        </ul>
+    </div>
+    <div class="menu">
+        <h3>Redes <span>sociales</span></h3>
+        <nav class="redes-sociales">
+          <a href="#"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+          <a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+          <a href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
+          <a href="#"><i class="fab fa-youtube" aria-hidden="true"></i></a>
+          <a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+        </nav>
+    </div>
+  </div>
+
+  <p class="copyright">Todos los derechos reservados &copyDevsBC 2018 </p>
